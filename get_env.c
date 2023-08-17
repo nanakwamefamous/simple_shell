4065e86a0584762cee8c1d27a01e04a24aa8b26f
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2023-08-17 22:02:06 +0000

    Update get_env.c

--- get_env.c
+++ get_env.c
@@ -4,7 +4,7 @@
  * _getenv - Get the value of an environment variable and return its value.
  * @env_name: Name of the environment variable
  *
- * Return: Value of the environment variable, or NULL if it doesn't exist. 
+ * Return: Value of the environment variable, or NULL if it doesn't exist.
  */
 char *_getenv(const char *env_name)
 {
@@ -13,7 +13,8 @@ char *_getenv(const char *env_name)
 
 	for (env_var = environ; *env_var != NULL; env_var++)
 	{
-		if (_strncmp(*env_var, env_name, MY_name_len) == 0 && (*env_var)[MY_name_len] == '=')
+		if (_strncmp(*env_var, env_name, MY_name_len) == 0 && (*env_var)
+			[MY_name_len] == '=')
 		{
 			return (&(*env_var)[MY_name_len + 1]);
 		}
