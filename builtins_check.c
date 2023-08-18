3b1813fa734946187222d775b962a34ef1afac2d
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2023-08-18 00:11:09 +0000

    Update builtins_check.c

--- builtins_check.c
+++ builtins_check.c
@@ -1,31 +1,31 @@
 #include "shell.h"
 
 /**
- * checkforbuiltin - checks if the command is a builtin.
- * @argsarray: arguments array
+ * check_for_builtin - checks if the command is a builtin.
+ * @args_array: arguments array
  *
  * Return: 1 if command is a builtin, 0 otherwise
  */
-int checkforbuiltin(char **argsarray)
+int check_for_builtin(char **args_array)
 {
-	if (!argsarray[0])
+	if (!args_array[0])
 		return (0);
-	if (_strcmp(argsarray[0], "exit"))
-		shell_exit(argsarray);
-	else if (_strcmp(argsarray[0], "env"))
+	if (_strcmp(args_array[0], "exit"))
+		shell_exit(args_array);
+	else if (_strcmp(args_array[0], "env"))
 		shell_env();
 	else if (_strcmp(args_array[0], "execute"))
 		execute();
 	else if (_strcmp(argsarray[0], "setenv"))
-		shell_setenv(argsarray);
-	else if (_strcmp(argsarray[0], "unsetenv"))
-		shell_unsetenv(argsarray);
-	else if (_strcmp(argsarray[0], "help"))
+		shell_setenv(args_array);
+	else if (_strcmp(args_array[0], "unsetenv"))
+		shell_unsetenv(args_array);
+	else if (_strcmp(args_array[0], "help"))
 		shell_help();
-	else if (_strcmp(argsarray[0], "cd"))
-		shell_cd(argsarray);
-	else if (_strcmp(argsarray[0], "clear"))
-		shell_clear(argsarray);
+	else if (_strcmp(args_array[0], "cd"))
+		shell_cd(args_array);
+	else if (_strcmp(args_array[0], "clear"))
+		shell_clear(args_array);
 	else
 		return (0);
 	return (1);
